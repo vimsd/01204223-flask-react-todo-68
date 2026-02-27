66dfb8fc08f18af5f40bc89c9156c5a067908cc3
vimsd <cheertahh@gmail.com>
2026-02-27 14:16:47 +0700

    ทดสอบ App

--- frontend/src/__tests__/App.test.jsx
+++ frontend/src/__tests__/App.test.jsx
@@ -8,6 +8,19 @@ const mockResponse = (body, ok = true) =>
     json: () => Promise.resolve(body),
 });
 
+// *** ย้ายมาที่นี่
+const todoItem1 = { id: 1, title: 'First todo', done: false, comments: [] };
+const todoItem2 = { id: 2, title: 'Second todo', done: false, comments: [
+  { id: 1, message: 'First comment' },
+  { id: 2, message: 'Second comment' },
+] };
+
+const originalTodoList = [
+  todoItem1,
+  todoItem2,
+]
+
+
 describe('App', () => {
   beforeEach(() => {
     vi.stubGlobal('fetch', vi.fn());
@@ -20,13 +33,7 @@ describe('App', () => {
 
   it('renders correctly', async () => {
     global.fetch.mockImplementationOnce(() =>
-      mockResponse([
-        { id: 1, title: 'First todo', done: false, comments: [] },
-        { id: 2, title: 'Second todo', done: false, comments: [
-          { id: 1, message: 'First comment' },
-          { id: 2, message: 'Second comment' },
-        ] },
-      ]),
+	    mockResponse(originalTodoList)
     );
 
     render(<App />);
@@ -35,5 +42,31 @@ describe('App', () => {
     expect(await screen.findByText('Second todo')).toBeInTheDocument();
     expect(await screen.findByText('First comment')).toBeInTheDocument();
     expect(await screen.findByText('Second comment')).toBeInTheDocument();
+  });
+
+  it('toggles done on a todo item', async() => {
+    // เตรียมค่าสำหรับคืนหลังกด toggle done แล้ว
+    const toggledTodoItem1 = { ...todoItem1, done: true };
+
+    // mock fetch --- สังเกตว่าจะมีการเรียก fetch สองครั้ง จากการ init และจากการกดปุ่ม 
+    //   สำหรับการเรียกแต่ละครั้งเราจะสามารถโปรแกรมคำตอบแยกกันได้ โดยเรียก mockImplementationOnce หลายครั้ง
+    //   กล่าวคือ รอบแรกคืนรายการทั้งหมด  รอบที่สองคืนค่า todo item ที่แก้ค่าแล้ว
+    global.fetch
+      .mockImplementationOnce(() => mockResponse(originalTodoList))    
+      .mockImplementationOnce(() => mockResponse(toggledTodoItem1));
+
+    render(<App />);
+
+    // assert ก่อนว่าของเดิม todo item แรกไม่ได้มีคลาส done
+    expect(await screen.findByText('First todo')).not.toHaveClass('done');
+
+    // หาปุ่ม จะเจอ 2 ปุ่ม (เพราะว่ามี 2 todo item)
+    const toggleButtons = await screen.findAllByRole('button', { name: /toggle/i })
+    // เลือกกดปุ่มแรก
+    toggleButtons[0].click();
+
+    // ตรวจสอบว่า todo item นั้นเปลี่ยนคลาสเป็น done แล้ว
+    expect(await screen.findByText('First todo')).toHaveClass('done');
+    expect(global.fetch).toHaveBeenLastCalledWith(expect.stringMatching(/1\/toggle/), { method: 'PATCH' });
   });
 });
